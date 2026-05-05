2ea0696132ef4a1f2298c37cc658a9c1c937a810
Thaislaa <thaisladaveiga@gmail.com>
2026-05-05 08:33:29 -0300

    adiciona rotas de criação, listagem, atualização e remoção de jogos

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,101 @@
 import express from "express";
+import { JogoRepository } from "./repositories/jogo.repository.js";
 
 const app = express()
 app.use(express.json())
+
+const jogoRepository = new JogoRepository()
+
+// -------- JOGOS --------
+
+// LISTAR JOGOS
+app.get("/jogos", async (req, res) => {
+    const jogos = await jogoRepository.listar()
+    return res.status(200).send({
+        ok: true,
+        message: "Jogos listados com sucesso",
+        data: jogos
+    })
+})
+
+// CRIAR JOGO
+app.post("/jogos", async (req, res) => {
+    const { nome, genero, tamanho, preco, dtLancamento, multiplayer } = req.body
+
+    if (!nome || !genero || !dtLancamento || multiplayer === undefined || tamanho === undefined || preco === undefined) {
+        return res.status(400).send({
+            ok: false,
+            message: "Campos não foram informados corretamente"
+        })
+    }
+
+    const jogo = await jogoRepository.criar({
+        nome,
+        genero,
+        tamanho,
+        preco,
+        multiplayer,
+        dtLancamento: new Date(dtLancamento)
+    })
+    return res.status(201).send({
+        ok: true,
+        message: "Jogo criado com sucesso",
+        data: jogo
+    })
+})
+
+// ATUALIZAR JOGO
+app.put("/jogos/:idJogo", async (req, res) => {
+    const { idJogo } = req.params
+    const { nome, genero, tamanho, preco, dtLancamento, multiplayer } = req.body
+
+    if (!nome && !genero && !dtLancamento && multiplayer === undefined && tamanho === undefined && preco === undefined) {
+        return res.status(400).send({
+            ok: false,
+            message: "Informe pelo menos um campo"
+        })
+    }
+
+    const encontrado = await jogoRepository.obterPorId(idJogo)
+    if (!encontrado) {
+        return res.status(404).send({
+            ok: false,
+            message: "Jogo não encontrado"
+        })
+    }
+
+    const jogo = await jogoRepository.atualizar(idJogo, {
+        ...req.body,
+        dtLancamento: dtLancamento ? new Date(dtLancamento) : undefined
+    })
+
+    return res.status(200).send({
+        ok: true,
+        message: "Jogo atualizado com sucesso",
+        data: jogo
+    })
+})
+
+// DELETAR JOGO
+app.delete("/jogos/:idJogo", async (req, res) => {
+    const { idJogo } = req.params
+
+    const encontrado = await jogoRepository.obterPorId(idJogo)
+
+    if (!encontrado) {
+        return res.status(404).send({
+            ok: false,
+            message: "Jogo não encontrado"
+        })
+    }
+
+    const jogo = await jogoRepository.deletar(idJogo)
+    return res.status(200).send({
+        ok: true,
+        message: "Jogo deletado com sucesso",
+        data: jogo
+    })
+})
 
 app.listen(3333, () => {
     console.log("API está rodando na porta 3333")
